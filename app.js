650f4bd8986df61ef048e2d9e891b557273f5fe6
Franciska Englert <englert.franciska@gmail.com>
2023-04-14 18:36:06 +0100

    Routes implemented

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 var app = express();
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb+srv://m001-student:m001-student@sandbox.hfwii.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
